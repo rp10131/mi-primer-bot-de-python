2c2b4b5e634f08a2c1f675444d925ccf9568907b
tbhkritur <rebeca_pamela_2010@hotmail.com>
2025-03-27 09:39:16 -0600

    comentarios para bot.py
    Ajustes de comentarios para facilitar la lectura (supongo).

--- bot.py
+++ bot.py
@@ -1,73 +1,73 @@
-import discord, random, os, asyncio 
+import discord, random, os, asyncio # "os" es el sistema operativo. Asyncio: https://docs.python.org/3/library/asyncio.html
 from discord.ext import commands, tasks # tasks se usa para mensajes programados/automatizados.
 from dotenv import load_dotenv # info: https://www.geeksforgeeks.org/using-python-environment-variables-with-python-dotenv/
 from datetime import datetime, timedelta # paquete que se usa para obtener meses, el año, hora, etc.
 
-# Cargar las variables del archivo .env y funciones
+# Cargar las variables del archivo .env y funciones.
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN") # Obtiene la variable DISCORD_TOKEN del archivo .env
 
-# La variable intents almacena los privilegios del bot
+# La variable intents almacena los privilegios del bot que el desarrollador le puso por defecto.
 intents = discord.Intents.default()
-# Activar el privilegio de lectura de mensajes
+# Activar el privilegio de lectura de mensajes.
 intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
-client = commands.Bot(command_prefix='y-', intents=intents)
+# Crear un bot en la clase Bot (commands.Bot) y transferirle los privilegios.
+client = commands.Bot(command_prefix='y-', intents=intents) # "client" puede tener cualquier nombre.
 last_message_time = datetime.now()
 
 @client.event
-async def on_ready():
+async def on_ready(): # Cuando se abre el archivo .py del código del bot en la consola.
     juego = ['Ping Pong', 'Piedra, Papel o Tijera', 'Sopa de Tortuga']
     viendo = ['Discord', 'la fortuna de los miembros conectados', 'y-y.help']
     haciendo = random.randint(1,2) # Escoge la actividad que aparece en el estado del bot. 
     if haciendo == 1:
-        activity = discord.Game(name=random.choice(juego))
+        activity = discord.Game(name=random.choice(juego)) # El estado es "Jugando a _".
     else:
-        activity = discord.Activity(type=discord.ActivityType.watching, name=random.choice(viendo))
+        activity = discord.Activity(type=discord.ActivityType.watching, name=random.choice(viendo)) # El estado es "Viendo _".
 
-    await client.change_presence(activity=activity)
-    print(f'Hemos iniciado sesión como {client.user}\n{activity}')
+    await client.change_presence(activity=activity) # El estado se activa.
+    print(f'Hemos iniciado sesión como {client.user}\n{activity}') # El bot está en línea.
     check_inactivity.start() # Comienza a verificar si ha pasado mucho tiempo desde el último mensaje.
 
 @client.event
-async def on_message(message):
+async def on_message(message): # Cuando hay mensajes en el servidor de Discord.
     global last_message_time
 
     if message.author == client.user:
         return # No queremos que el bot responda a sus propios mensajes.
     else:
         last_message_time = datetime.now() # La fecha y hora de la última vez que alguien envió un mensaje.
-    if message.content.startswith('hola'):
-        await message.channel.send("Hola!")
+    if message.content.startswith('hola'): # Mensaje de alguien.
+        await message.channel.send("Hola!") # Respuesta del bot.
     elif message.content.startswith('adiós'):
         await message.channel.send("\U0001f642") # Emoji en unicode.
     elif message.content.startswith('!ping'):
         await message.channel.send("Pong!")
     if (message.content == 'y-tortuga'): # El comando cuando no se le dan argumentos.
-        await message.add_reaction("\U0001F422")
+        await message.add_reaction("\U0001F422") # Añade una reacción al mensaje del usuario.
         await message.channel.send("Un hombre pidió sopa de tortuga en un restaurante con vista al mar. El hombre tomó un sorbo de sopa, confirmó que era auténtica sopa de tortuga, pagó la cuenta, se fue a casa y luego se suicidó. ¿Por qué?")
         await message.channel.send("Cómo jugar: Ingresa tu pregunta con el siguiente formato: y-tortuga <preguna>. Yumemibot responderá con 'Sí' o 'No' o 'Es irrelevante'.")
     if message.content == 'y-piedrapapel': # El comando cuando no se le dan argumentos.
         await message.channel.send("Juguemos Piedra Papel o Tijera! Tu mensaje debe verse así: y-piedrapapel <opción>")
 
-    await client.process_commands(message)
+    await client.process_commands(message) # Esto es necesario para que el bot pueda responder a comandos además de a eventos.
 
-@tasks.loop(seconds=60)  # Verifica cada minuto
+@tasks.loop(seconds=60)  # Clase Tasks. Verifica cada minuto.
 async def check_inactivity():
     global last_message_time
-    current_time = datetime.now() # Hora y fecha actuales
+    current_time = datetime.now() # Hora y fecha actuales.
     if current_time - last_message_time > timedelta(minutes=60): # Si ha pasado más de una hora desde el último mensaje.
         channel = discord.utils.get(client.get_all_channels(), name="nombre-del-canal") # Se puede cambiar el nombre del canal.
         if channel:
             await channel.send("¡Hola! Parece que hace rato no hay mensajes 😊")
-            print(last_message_time)
+            print(last_message_time) # En la consola aparece la fecha del último mensaje.
             last_message_time = datetime.now() # El mensaje del bot se convierte en el más reciente y se guardan la fecha y hora.
 
 @client.command(name="piedrapapel", help='juega al juego Piedra Papel o Tijera; puede funcionar con solo "y-piedrapapel"')
-async def piedra_papel_tijera(ctx, eleccion: str): # "eleccion: str" es un argumentos que tiene que introducir el usuario.
+async def piedra_papel_tijera(ctx, eleccion: str): # "eleccion" es un argumento string que tiene que introducir el usuario.
     opciones = ["piedra", "papel", "tijera"]
-    if eleccion.lower() not in opciones: # El argumento no está en la lista.
+    if eleccion.lower() not in opciones: # Si el argumento no está en la lista.
         await ctx.send("Por favor, elige entre piedra, papel o tijera.")
         return
     elif eleccion.lower() == 'yumemi': # Diversión.
@@ -77,19 +77,19 @@ async def piedra_papel_tijera(ctx, eleccion: str): # "eleccion: str" es un argum
     eleccion_bot = random.choice(opciones)
     mensaje = f"🤖 Yo elijo **{eleccion_bot}**. Tú eliges **{eleccion.lower()}**." # La elección del bot y el usuario. En minúsculas.
 
-    if eleccion.lower() == eleccion_bot:
+    if eleccion.lower() == eleccion_bot: # Empate.
         resultado = "¡Empatamos! :o"
     elif (eleccion.lower() == "piedra" and eleccion_bot == "tijera") or \
          (eleccion.lower() == "papel" and eleccion_bot == "piedra") or \
          (eleccion.lower() == "tijera" and eleccion_bot == "papel"):
         resultado = "¡Ganaste! :)" # Verifica si el usuario ganó.
-    else:
+    else: # El usuario perdió.
         resultado = "¡Perdiste! ;v;"
 
     await ctx.send(f"{mensaje}\n{resultado}")
 
 @client.command(name="tortuga", help='juega al juego Sopa de Tortuga; puede funcionar con solo "y-tortuga"')
-async def sopa_de_tortuga(ctx, eleccion: str): # "elección: str" es un argumento que puede tener cualquier valor.
+async def sopa_de_tortuga(ctx, eleccion: str): # "elección" es un argumento string que puede tener cualquier valor.
     opciones = ['Sí', 'No', 'Es irrelevante']
     if eleccion != '': # si hay un argumento:
         await ctx.send(f"Creo que.. {random.choice(opciones)}") # Responde con un objeto de la lista.
@@ -132,8 +132,8 @@ async def heh(ctx, count_heh = 5): # El argumento por defecto es 5. Se puede cam
 @client.command(name='adivinar', help='Escoge un número del 1 al 10 y el usuario tiene que adivinar cuál es.')
 async def adivinar(ctx):
     await ctx.send('Adivina un número entre el 1 y el 10!')
-    def is_correct(m):
-        return m.author == ctx.author and m.content.isdigit()
+    def is_correct(m): # Cuando hay mensaje (m) de respuesta para adivinar:
+        return m.author == ctx.author and m.content.isdigit() # El autor del mensaje es el del comando; el mensaje es un número.
 
     answer = random.randint(1, 10)
 
@@ -152,7 +152,7 @@ async def lista_comandos(ctx):
     embed = discord.Embed(title="✯y u me m i b o t ・ c o m m a n d s✯", description="Aquí están los comandos organizados por categorías:", color=discord.Color.blue())
     
     # Categoría: Otro
-    embed.add_field(name="**Otros**", value="`saludo` - Envía un saludo amistoso así nada más.\n `embed` - envía un embed vacío", inline=False)
+    embed.add_field(name="**Otros**", value="`saludo` - Envía un saludo amistoso así nada más., inline=False)
     
     # Categoría: Diversión
     embed.add_field(name="**Diversión**", value="`fortuna` - Predice tu fortuna en un futuro cercano.\n `piedrapapel` - juega al juego Piedra Papel o Tijera; puede funcionar con solo 'y-piedrapapel'\n `tortuga` - juega al juego Sopa de Tortuga; puede funcionar con solo 'y-tortuga'\n `heh` - risas. Es posible cambiar la cantidad de 'heh' con 'y-heh <número>'", inline=False)
@@ -160,4 +160,4 @@ async def lista_comandos(ctx):
 
     await ctx.send(embed=embed)
 
-client.run(TOKEN) 
+client.run(TOKEN) # El bot se pone en marcha.
