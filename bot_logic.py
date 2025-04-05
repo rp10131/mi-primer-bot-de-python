e032728671122e62aff5df9ac26fbf33bc17a9bb
tbhkritur <rebeca_pamela_2010@hotmail.com>
2025-04-04 18:23:47 -0600

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,27 +1,20 @@
 import random, requests,os
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
-
-    for i in range(pass_length):
-        password += random.choice(elements)
-
-    return password
-
 # comando "y-animal"
 def get_image(animal):
-    if animal == None:
+    if animal == None: # Si no especifica el animal:
+        # Obtiene un valor aleatorio de cada función que obtiene urls.
         animales = [obtener_url_de_pato(), obtener_url_de_perro(), obtener_url_de_zorro(), obtener_url_de_gato(),obtener_url_de_oso()]
-        validos = [img for img in animales if img]
+        validos = [img for img in animales if img] # Revisa las imágenes que sí se obtuvieron.
         if not validos:
             return "No se pudo obtener una imagen en este momento."
-        return random.choice(validos)
-    elif animal in ['pato', 'perro', 'zorro', 'gato', 'oso']:
-        return globals()[f"obtener_url_de_{animal}"]()  # Llama dinámicamente la función correspondiente
+        return random.choice(validos) # Regresa a bot.py una de las imágenes obtenidas.
+    elif animal in ['pato', 'perro', 'zorro', 'gato', 'oso']: # Cuando el animal especificado sí está ahí.
+        return globals()[f"obtener_url_de_{animal}"]()  # Llama dinámicamente la función correspondiente.
     else:
-        return f"Lo siento, no tengo imágenes para '{animal}'. Prueba con pato, perro, zorro, gato u oso 🐻"
+        return f"Lo siento, no tengo imágenes para '{animal}'. Prueba con pato, perro, zorro, gato u oso 🐻" # No se encontró el animal.
 
+# APIs exteriores.
 def obtener_url_de_pato():    
     url = 'https://random-d.uk/api/random'
     res = requests.get(url)
@@ -41,35 +34,35 @@ def obtener_url_de_zorro():
     return data['link']
 
 def obtener_url_de_gato():
-    url = 'https://cataas.com/cat'
+    url = 'https://cataas.com/cat' # El enlace automaticamente devuelva fotos de gatos aleatorias.
     return url
 
-def obtener_url_de_oso():
+def obtener_url_de_oso(): # Las imagenes cambian dependiedo de las medidas.
     x = random.randint(300,500)
     y = random.randint(300,500)
     url = f"https://placebear.com/{x}/{y}"
     return url
-
+###
 # comandos de y-meme
 def meme(estilo):
     global estilo_2
-    estilo_2 = estilo
+    estilo_2 = estilo # almacena el estilo que recibió´la función para poder usarla en otro lugar.
 
-    memes = os.listdir(f"images/{estilo}")
-    img_name = random.choice(memes)
-    return probabilidad(img_name)
+    memes = os.listdir(f"images/{estilo}") # obtiene las imagenes en la subcarpeta.
+    img_name = random.choice(memes) # escoge una de esas imágenes.
+    return probabilidad(img_name) # la función "probabilidad" decide si enviar la imagen o no.
 
 def probabilidad(imagen):
     quinto_de_probabilidad = [
         'microondas.webp', 'Screenshot_20241129_182944.webp', 
         'IMG_2727.webp','d99fe7080655645c6367d908f443760f.webp', 'plan_incre.webp'
-    ]
+    ] # imágenes con un quinto de probabilidad de que se envien
     if imagen in quinto_de_probabilidad:
         si_o_no = random.randint(1,5)
-        if si_o_no != 1:
+        if si_o_no != 1: # la imagen no se envía.
             print(f"Se hizo la probabilidad ({si_o_no})|{imagen}" )
-            return meme(estilo_2)
-        else:
+            return meme(estilo_2) # llama a la función "meme" para escoja una nueva imagen.
+        else: # la imagen se envía.
             print(si_o_no)
             return imagen
     else:
